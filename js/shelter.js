ffbd7702eeceeeccca61a61264e4464e01c15cb3
rakion99 <_rakion99@outlook.com>
2019-06-03 23:18:21 -0500

    removed outfit remaining code
    * removed outfit color as don't work

--- js/shelter.js
+++ js/shelter.js
@@ -222,7 +222,6 @@ app.controller('dwellerController', function ($scope) {
     _vaultName = -1,
     _skinColor = null,
     _hairColor = null,
-    _outfitColor = null,
     _firstName = null;
 
     Object.defineProperty($scope, 'firstName', {
@@ -274,16 +273,6 @@ app.controller('dwellerController', function ($scope) {
       set: function (val) {
         _hairColor = val;
         $scope.dweller.hairColor = colorConverter(val);
-      }
-    });
-    
-    Object.defineProperty($scope, 'outfitColor', {
-      get: function () {
-        return _outfitColor
-      },
-      set: function (val) {
-        _outfitColor = val;
-        $scope.dweller.outfitColor = colorConverter(val);
       }
     });
 
@@ -382,7 +371,6 @@ app.controller('dwellerController', function ($scope) {
     _firstName = $scope.dweller.name;
     _skinColor = colorConverter($scope.dweller.skinColor, true);
     _hairColor = colorConverter($scope.dweller.hairColor, true);
-    _outfitColor = colorConverter($scope.dweller.outfitColor, true);
     setTimeout(colorHack, 200);
   };
 
